cfc0f7575ca8ea3f37d9e92af5cc2d3afe3c215a
Mickeyrourkeske <mickeyrourkeske@0011011.org>
2012-12-19 12:05:04 +0100

    GPL added to every source file

--- src/org/hs/pforzheim/ti/rlrc/Main.java
+++ src/org/hs/pforzheim/ti/rlrc/Main.java
@@ -1,5 +1,18 @@
 /**
+ * This file is part of RLRC.
  * 
+ * RLRC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RLRC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with RLRC.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.hs.pforzheim.ti.rlrc;
 
